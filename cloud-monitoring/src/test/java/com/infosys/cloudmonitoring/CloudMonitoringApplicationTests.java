12e31327a69e3fec7d37b1c3430294e5e6c062ec
pallaviemankar2007-a11y <pallaviemankar2007@gmail.com>
2026-08-26 12:25:46 +0530

    Initial Spring Boot project setup

---
+++ cloud-monitoring/src/test/java/com/infosys/cloudmonitoring/CloudMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infosys.cloudmonitoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudMonitoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
